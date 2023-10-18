cc38a143043856cc147308afb5f1374912a8bdbf
king <tudor1103@gmail.com>
2023-10-18 21:17:05 +0300

    2nd commit to test if all good

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. First commit
+          Edit <code>src/App.js</code> and save to reload. First commit, 2nd commit to test if all good
         </p>
         <a
           className="App-link"
