ac4146dc3a4ac28de674cd410d3920a9d71fb1c6
king <tudor1103@gmail.com>
2023-10-18 23:40:34 +0300

    started my react journey

--- src/index.js
+++ src/index.js
@@ -5,11 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const anotherElement = ReactDOM.createRoot(document.getElementById('anotherElement'));
+// root.render(
+//   <React.StrictMode>
+//       <h1>Hello world</h1>
+//   </React.StrictMode>
+// );
+
+var smthICanChange = 2;
+const aLotOfStuff = "<h1>" + smthICanChange + "</h1>";
+
+root.render(<h1>This is a fucking test</h1>);
+anotherElement.render("<div>This is another fucking test with smth dynamic:</div>" + aLotOfStuff);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
