5f007ae3408ea32abd73101053a029bc7499bebf
king <tudor1103@gmail.com>
2023-12-11 14:37:55 +0200

    small changes and started working with components

--- src/index.js
+++ src/index.js
@@ -1,119 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import react from "react"
+import reactDOM from "react-dom"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-function Navbar() {
+function Main() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                               data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <span className="dropdown-divider"></span>
-                                </li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <>
+            <Header />
+            <Content />
+            <Footer />
+        </>
     )
 }
 
-function MainContent() {
+function Header() {
     return (
-        <div className="main-content-container">
-            {test}
-        </div>
+        <header>
+            <nav>
+                <div className="img-container">
+                    <img style={{width: "50px"}} src="./mail-icon.png" />
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+function Content() {
+    return (
+        <>
+        <h1>Why I'm excited to learn react :)</h1>
+        <ol>
+            <li>Better job</li>
+            <li>More money</li>
+            <li>Personal growth</li>
+        </ol>
+        </>
     )
 }
 
 function Footer() {
     return (
-        <div className="footer-data d-flex">
-            <div className="col-md-4">
-                <ul className="footer-list">
-                    <li>item 1</li>
-                    <li>item 2</li>
-                    <li>item 3</li>
-                </ul>
-            </div>
-            <div class="col-md-4">
-                <ul className="footer-list">
-                    <li>item 1</li>
-                    <li>item 2</li>
-                    <li>item 3</li>
-                </ul>
-            </div>
-            <div class="col-md-4">
-                <ul className="footer-list">
-                    <li>item 1</li>
-                    <li>item 2</li>
-                    <li>item 3</li>
-                </ul>
-            </div>
+        <div>
+            <h3>Here is the footer</h3>
+            <p>@2023 website</p>
         </div>
     )
 }
 
-var test = "test" + 1
-
-/* Append an element to an id */
-const h1 = document.createElement("h1")
-h1.textContent = "Test test"
-h1.className = "header"
-//document.getElementById("anotherElement").append(h1)
-
-const page = (
-    //Use this to not leave any tag in DOM
-    <>
-        <h1>const element title</h1>
-        <p>const element paragraph</p>
-    </>
+reactDOM.render(
+    <Main />,
+    document.getElementById("root")
 )
-
-console.log(page);
-
-root.render(
-    <React.StrictMode>
-        <h1 id="header">Hello world</h1>
-        <Navbar />
-        <MainContent />
-        {page}
-        <Footer />
-    </React.StrictMode>
-);
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
