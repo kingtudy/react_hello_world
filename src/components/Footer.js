eaf9c030654cb3f12c49cdf2f85aa1830d017497
Tudor Ghiorghiu <tudor.ghiorghiu@xfactorapp.com>
2023-12-19 10:15:19 +0200

    added img import

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,9 @@
+import reactLogo from "../logo.svg"
+
 export default function Footer() {
     return (
         <div>
-            <img className="footer-img" alt="footer-img"  src="./react-logo.png" />
+            <img className="footer-img" alt="footer-img"  src={reactLogo} />
             <h3 className="white">Here is the footer</h3>
             <p className="white">@2023 website</p>
         </div>
